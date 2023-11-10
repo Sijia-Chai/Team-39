55a4b66581b89d48d899fd511308b21f53a400f6
FrankHe0506 <111326087+FrankHe0506@users.noreply.github.com>
2023-11-10 10:45:30 -0500

    Update api_keys.py

--- src/api_keys.py
+++ src/api_keys.py
@@ -1,2 +1,6 @@
+NOAA_api_token = 'htOHiXdrJuOvOCiePVaHwviVwfKwRuRY'
+
+Eventbrite_api_token = 'HIVMDJ4BER24AE5B4TLW'
+
 #google sign-in Ouath
 google_id="27975993888-scabcoq2dtujta3l2i7bmh9p02bc922g.apps.googleusercontent.com"
