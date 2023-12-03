77edefe06203ce91bbeb3b83a57ee8e8924703c0
FrankHe0506 <111326087+FrankHe0506@users.noreply.github.com>
2023-12-03 12:47:53 -0500

    Update methods.py

--- src/methods.py
+++ src/methods.py
@@ -1,25 +1,23 @@
 import requests
 import json
 
-weather_key = "e841e7921b15804eaae940ab17328077"
+google_maps_key = 'secret'
+weather_key = 'secret'
 
 def call_weather_api(location: str):
-  api_url = 'https://api.openweathermap.org/data/2.5/weather?q='+location+'&APPID='+weather_key+'&units=imperial'
-  return requests.get(api_url)
+  return requests.get('https://api.openweathermap.org/data/2.5/weather?q='+location+'&APPID='+weather_key+'&units=imperial')
 
-# Weather API
-
-@app.route("/weather", methods=['GET'])
-def weather():
-    return render_template('weather.html', supress='True')
-
-@app.route('/weather', methods=['POST'])
-def getWeather():
-    if request.method == 'POST':
-        response = call_weather_api(request.form.get('city'))
-    # checking the status code of the request
-        if response.status_code == 200:
-            return render_template('weather_ret.html', city=request.form.get('city'), temperature=response.json()['main']['temp'], feels_like=response.json()['main']['feels_like'])
-        else:
-            # showing the error message
-            return render_template('weather.html') + "<h3 style = \"color: #ff0000\">An error occured loading up the weather for the city provided</h3>"
+def call_map_api(location: str):
+    response = json.loads(requests.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query="+("fun things to do in "+ location)+"&key="+google_maps_key).text)['results']
+    List = []
+    for entry in response:
+        Dict = {}
+        Dict['name'] = entry['name']
+        Dict['address'] = entry['formatted_address']
+        Dict['rating'] = entry['rating']
+        tmp = ""
+        for i in entry['types']:
+            tmp += i + ", "
+        Dict['category'] = tmp.replace("_"," ")[:-2]
+        List.append(Dict)
+    return json.loads(json.dumps(List))
