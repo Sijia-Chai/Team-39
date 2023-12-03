b5be5321bb69996a666ae79f4150f5f0dd4452db
FrankHe0506 <111326087+FrankHe0506@users.noreply.github.com>
2023-12-03 12:50:53 -0500

    Update api_keys.py

--- src/api_keys.py
+++ src/api_keys.py
@@ -1,6 +1,5 @@
-NOAA_api_token = 'htOHiXdrJuOvOCiePVaHwviVwfKwRuRY'
-
-Eventbrite_api_token = 'HIVMDJ4BER24AE5B4TLW'
-
-#google sign-in Ouath
+google_maps_key = 'secret'
+weather_key = 'secret'
+my_sql_database_key = 'secret'
+flask_app_secret = 'secret'
 google_id="27975993888-scabcoq2dtujta3l2i7bmh9p02bc922g.apps.googleusercontent.com"
