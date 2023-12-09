cd51c50c9362ac9fdc7a459e220dd7de684088bb
FrankHe0506 <111326087+FrankHe0506@users.noreply.github.com>
2023-12-09 13:18:10 -0500

    Update app.py

--- src/app.py
+++ src/app.py
@@ -15,24 +15,6 @@ import mysql.connector
 import re
 import sqlite3
 import os.path
- 
-import google.oauth2.id_token as id_token
-import google
-import pip._vendor.cachecontrol as cachecontrol
-import os
-import pathlib
-from google_auth_oauthlib.flow import Flow
-from flask import *
-from api_keys_public import *
-from methods import *
-import flask
-import requests
-import flask_login
-import webbrowser
-import mysql.connector
-import re
-import sqlite3
-import os.path
 
 app = Flask(__name__)
 app.secret_key = flask_app_secret
